1e9bcbb12bdc4fe45c879737bb6b9dac0b8ef477
James Xue <jamesxue100@gmail.com>
2016-09-24 09:37:05 -0400

    Python Flask boilerplate

---
+++ app.py
@@ -0,0 +1,20 @@
+import httplib2
+
+
+from flask import Flask, render_template, json, request, redirect
+
+app = Flask(__name__)
+app.config['DEBUG'] = True 
+
+@app.route('/')
+def main():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object and outputs a list of the next
+    10 events on the user's calendar.
+    """
+    return render_template('index.html', api_data="")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
