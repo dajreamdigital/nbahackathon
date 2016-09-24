90a5b9d4059ed03201849411215b8a984f070f8d
James Xue <jamesxue100@gmail.com>
2016-09-24 14:50:53 -0400

    added more data i.e. warriors

--- app.py
+++ app.py
@@ -47,10 +47,19 @@ def search():
 #Creates JSON for all handles in list 
 @app.route('/search_all', methods = ['GET','POST'])
 def search_all():
-    handles = ["KingJames", "Sirdom1", "JordyMac52", "2kayzero", "TheRealJRSmith", "KyrieIrving", "Channing_Frye", "imanshumpert", "kevinlove", "RealTristan13", "mowilliams"]
+    #2014-2015 source: http://www.usatoday.com/story/sports/nba/2014/10/27/nba-teams-announced-their-opening-day-rosters-for-the-2014-15-season/18035777/
+    handles = ["KingJames", "KyrieIrving", "kevinlove", "Sirdom1", "JordyMac52", "2kayzero", "TheRealJRSmith", "Channing_Frye", "imanshumpert", "RealTristan13", "mowilliams"]
+    # handles = ["StephenCurry30", "Money23Green", "hbarnes", "KlayThompson", "festus", "TheBlurBarbosa", "JustHolla7", "andre", "ShaunLivingston", "Dlee042", "Mospeights16", "BRush_25"]
+    # handles = []
+    # handles = ["DeMar_DeRozan", "Klow7", "Cory_Joe", "JValanciunas", "T_DotFlight31", "npowell2404", "pdpatt", "jtthekid", "pskills43", "Bebe92", "Bruno_Caboclo", "EJSingler", "delonwright", "bradyheslip"]
+    # handles = ["gallinari8888", "JaValeMcGee34", "KennethFaried35", "emmanuelmudiay", "wilsonchandler", "nurkic23", "MikeMiller_13", "thats_G_", "WillTheThrillB5", "DarrellArthur00", "jameernelson", "bigplaydj1", "Mbeasy5", "NateWolters", "JarnellStokes",]
+    # handles = ["KDTrey35", "russwest44", "RealStevenAdams", "VicOladipo", "Enes_Kanter", "Dsabonis11", "FlyDre21", "alexabrines", "campayne", "1JOLOLO", "KyleSingler", "nickcollison4", "MrAnthonyMorrow", "jhuestis", "NazrMohammed", "DakariJohnson"]
+    # handles = ["antic12pero", "24Bazemore", "DeMarreCarroll1", "Al_Horford", "JohnnyCashVU23", "ShelvinMack", "Paulmillsap4", "mikemuscala", "Adreian_Payne", "DennisMike93", "mikescott", "ThaboSefolosha", "Teague0", "KyleKorver", "TheRealEB42"]
+    # handles = ["bestbetbass", "unclejeffgreen", "KellyOlynyk", "philpressey", "RajonRondo", "smart_MS3", "Jared _Sully0", "OfficialMT23", "thekidet", "realjamesyoung", "ZellerTyler", "DwightPowell33"]
+
     playerlist = []
     for handle in handles:
-        since = "2015-09-21"
+        since = "2015-09-21" #arbitrary date way in the past
         count = 0
 
         return_dict = {}
