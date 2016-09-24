a37ef58b5cce1bd4d462f3dacc9547d6dd3815ae
dodiku <ayalon.dror@gmail.com>
2016-09-24 15:51:49 -0400

    charts are on

--- static/nbachart.js
+++ static/nbachart.js
@@ -1,30 +1,44 @@
-console.log('cool');
 
-var pieData = [
-   {
-      value: 25,
-      label: 'Java',
-      color: '#811BD6'
-   },
-   {
-      value: 10,
-      label: 'Scala',
-      color: '#9CBABA'
-   },
-   {
-      value: 30,
-      label: 'PHP',
-      color: '#D18177'
-   },
-   {
-      value : 35,
-      label: 'HTML',
-      color: '#6AE128'
-   }
-];
-
-console.log(pieData);
-
+var skillsData = {
+	labels : ["January","February","March","April","May","June"],
+	datasets : [
+		{
+			fillColor : "rgba(172,194,132,0.4)",
+			strokeColor : "#ACC26D",
+			pointColor : "#fff",
+			pointStrokeColor : "#9DB86D",
+			data : [203,156,99,251,305,247]
+		},
+    {
+			fillColor : "rgba(132,194,255,0.4)",
+			strokeColor : "#ACC26D",
+			pointColor : "#fff",
+			pointStrokeColor : "#9DB86D",
+			data : [100,10,20,123,200,300]
+		},
+	]
+};
 
 var context = document.getElementById('skills').getContext('2d');
-var skillsChart = new Chart(context).Pie(pieData);
+var skillsChart = new Chart(context).Radar(skillsData);
+
+var skillsChart = new Chart(context, {
+    type: 'radar',
+    data: skillsData,
+    // options: options
+});
+
+var tweetData = {
+	labels : ["Team Avg. Twitter Activity","Player Twitter Activity"],
+	datasets : [
+		{
+      fillColor : "rgba(73,188,170,0.4)",
+			strokeColor : "rgba(72,174,209,0.4)",
+			data : [456, 123]
+		},
+	]
+};
+
+
+var ctx = document.getElementById('tweets').getContext('2d');
+var skillsChart = new Chart(ctx).Bar(tweetData);
