bf8402622ce3ca4a3221325f923a434de18650a0
dodiku <ayalon.dror@gmail.com>
2016-09-24 14:11:41 -0400

    charts.js

---
+++ static/nbachart.js
@@ -0,0 +1,30 @@
+console.log('cool');
+
+var pieData = [
+   {
+      value: 25,
+      label: 'Java',
+      color: '#811BD6'
+   },
+   {
+      value: 10,
+      label: 'Scala',
+      color: '#9CBABA'
+   },
+   {
+      value: 30,
+      label: 'PHP',
+      color: '#D18177'
+   },
+   {
+      value : 35,
+      label: 'HTML',
+      color: '#6AE128'
+   }
+];
+
+console.log(pieData);
+
+
+var context = document.getElementById('skills').getContext('2d');
+var skillsChart = new Chart(context).Pie(pieData);
